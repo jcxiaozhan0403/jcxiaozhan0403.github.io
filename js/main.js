4b0af884f0d516d65dc5d35dd6846a404ac4569b
John.Cena <349636607@qq.com>
2020-12-13 19:49:05 +0800

    Site updated: 2020-12-13 19:49:05

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
   */
 
 $(function () {
+  import 'default-passive-events';
+
   const blogNameWidth = $('#site-name').width()
   const menusWidth = $('#menus').width()
   const sidebarWidth = $('#sidebar').width() || 300
