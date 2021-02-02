496c9e9c2e3073332f23aa0b1e2dbfb7fa30701a
John.Cena <349636607@qq.com>
2021-02-02 22:15:56 +0800

    Site updated: 2021-02-02 22:15:55

--- js/myjs.js
+++ js/myjs.js
@@ -30,7 +30,7 @@ function setTime() {
     var create_time = Math.round(new Date(Date.UTC(2020, 0, 09, 0, 0, 0)).getTime() / 1000);
     var timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
     currentTime = secondToDate((timestamp - create_time));
-    currentTimeHtml = "小站艰难维持了：" + currentTime[1] + '天' + currentTime[2] + '时' + currentTime[3] + '分' + currentTime[4] + '秒';
+    currentTimeHtml = "小站艰难维持了：" + currentTime[0] + '年' + currentTime[1] + '天' + currentTime[2] + '时' + currentTime[3] + '分' + currentTime[4] + '秒';
     document.getElementById("htmer_time").innerHTML = currentTimeHtml;
 }    setInterval(setTime, 1000);
 
